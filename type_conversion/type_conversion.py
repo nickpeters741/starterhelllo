b44d1a2559ab27b26c4440cb1fcdf1016a06f767
npeters741@gmail.com <npeters741@gmail.com>
2023-09-09 18:40:18 +0300

    update

--- type_conversion/type_conversion.py
+++ type_conversion/type_conversion.py
@@ -1,3 +1,5 @@
 birth_year =input('birth year: ')
+print(type(birth_year))
 age = 2023 -int(birth_year) # i had to input int to convert a sting to integer by passing to int function
+print(type(age))
 print(age)
