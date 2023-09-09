f070d7d580862254534f75ab4496cb8cfce4b112
npeters741@gmail.com <npeters741@gmail.com>
2023-09-09 15:54:02 +0300

    updates

--- variables.py
+++ variables.py
@@ -1,3 +1,10 @@
 # used to temporary store data in a computer memory
-price = 10
+price = 20  # integers
+price = 50
+name = "nicolas" # string
+is_published = True  #boolean
+rating = 4.8  # floats
 print(price)
+print(rating)
+print(name)
+print(is_published)
